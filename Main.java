ed94962064d123f958a738a8b2462308b4330e79
Dena Alharbi <109204661+DenaAlharbi@users.noreply.github.com>
2023-09-26 16:53:49 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,9 @@
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.Iterator;
-
-
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        ArrayList<Container> containers= new ArrayList<>();
+        ArrayList<Container> containers = new ArrayList<>();
         System.out.println("Enter the number of objects: ");
         int numContainers = input.nextInt();
         System.out.println("Enter the max weight: ");
@@ -18,38 +13,31 @@ public class Main {
         for (int i = 0; i < numContainers; i++) {
             int user = input.nextInt();
             boolean added = false;
-            int index =0;
-            //System.out.print(containers);
-            while(!added && (index<containers.size())){
-                //System.out.print(containers);
+            int index = 0;
 
-                Container current= containers.get(index);
+            while (!added && index < containers.size()) {
+                Container current = containers.get(index);
                 added = current.addItem(user);
+
                 if(added){
                     current.addItem(user);
                 }
-                 if(!added){
+                if(!added){
                     index ++;
-                    }
                 }
-            if(!added){
-                Container newcontainer = new Container(maxUser);
-                newcontainer.addItem(user);
+            }
 
-                containers.add(newcontainer);
-                //System.out.print(containers);
-
+            if (!added) {
+                Container newContainer = new Container(maxUser);
+                newContainer.addItem(user);
+                containers.add(newContainer);
             }
         }
-        System.out.println("Number of the containers needed: " + containers.size());
-        for (int i=0; i<containers.size();i++){
-            Container c= containers.get(i);
-            System.out.println("Container"+ (i+1));
-            for (Integer s : c) {
-                System.out.print(s);
-            }
-
+        System.out.println("Number of containers needed: " + containers.size());
+        for (int i = 0; i < containers.size(); i++) {
+            Container c = containers.get(i);
+            System.out.println("Container " + (i + 1) + " contains objects with weights " + c.containWeights);
         }
 
     }
-}
+    }
